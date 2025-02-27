5cde100d0f95ca83fdf755aed3540dffbd994116
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-27 21:44:49 +0530

    is_even function return a bool true or false

--- day_1_7th_Feb_2025/src/main.rs
+++ day_1_7th_Feb_2025/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
-    let ans = sum(7,2);
+    let ans = is_even(2);
     print!("{}", ans)
 }
 
-
-fn sum(a:u32, b:u32) -> u32{
-    return a+b;
+fn is_even(a:u32) -> bool {
+   return a%2 == 0; 
 }
+
+// fn main() {
+//     let ans = sum(7,2);
+//     print!("{}", ans)
+// }
+// fn sum(a:u32, b:u32) -> u32{
+//     return a+b;
+// }
